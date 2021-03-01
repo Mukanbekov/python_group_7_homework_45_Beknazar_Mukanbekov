07e17e1c82062c553997a97ea5419f4bf636c02a
MB7 <beknazarmb@mail.ru>
2021-03-01 17:55:36 +0600

    модель содержить основные поля и данные. Сюда добавил поле description.

--- to_do_list/list_app/models.py
+++ to_do_list/list_app/models.py
@@ -10,6 +10,7 @@ status_choices = (
 class List(models.Model):
     list = models.CharField(max_length=200, null=False, blank=False, verbose_name='Текст')
     status = models.TextField(max_length=30, null=False, blank=False, choices=status_choices, default='new', verbose_name='Статус')
+    description = models.TextField(max_length=200, null=False, blank=False, verbose_name='Поле')
     created_at = models.DateField(verbose_name='Время создания', blank=True, null=True)
 
     class Meta:
